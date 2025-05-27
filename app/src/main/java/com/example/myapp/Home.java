a8268b6fd3c73cd828676b71f214024a8cadaf07
Gabriel2643 <gv.pacaldo@gmail.com>
2025-05-27 10:55:41 +0800

    update

--- app/src/main/java/com/example/myapp/Home.java
+++ app/src/main/java/com/example/myapp/Home.java
@@ -82,7 +82,7 @@ public class Home extends Fragment implements OnCartUpdateListener, productrecyc
                     productImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
                 }
 
-                productobject product = new productobject(id, name, price, quantity,overquantity, quantity, productImage);
+                productobject product = new productobject(id, name, price, price, quantity, overquantity, productImage);
                 Product.add(product);
             }
         }
